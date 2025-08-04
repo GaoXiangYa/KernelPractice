16a50532daec41d9c6380b513001633edd5f1d9c
GaoXiangYa <gx19991105@outlook.com>
2025-08-05 07:58:31 +0800

    add matmul_sharedmem_kernel

--- src/cuda/gemm/matmul.cu
+++ src/cuda/gemm/matmul.cu
@@ -1,4 +1,5 @@
 #include "matmul.cuh"
+#include <cstdio>
 #include <cuda_runtime.h>
 
 // a[m, n] * b[n, k] = c[m, k]
@@ -16,36 +17,55 @@ __global__ void matmul_native_kernel(const float *a, const float *b, float *c,
 }
 
 // 使用shared memory进行优化
+// a[m, n] * b[n, k] = c[m, k]
 template <int BLOCK>
-__global__ void matmul_sharedmemory_kernel(float *A, float *B, float *C, int M, int N,
-                                int K) {
-  const int tx = threadIdx.x;
-  const int ty = threadIdx.y;
-  const int bx = blockIdx.x;
-  const int by = blockIdx.y;
+__global__ void matmul_sharedmemory_kernel(const float *__restrict__ matrix_a,
+                                           const float *__restrict__ matrix_b,
+                                           float *__restrict__ matrix_c, int m,
+                                           int n, int k) {
+  // dynamic allocate shared memory
+  extern __shared__ float shared_mem[];
+  float *shmem_a = shared_mem;
+  float *shmem_b = &shared_mem[BLOCK * BLOCK];
 
-  float *begin_a = A + by * BLOCK * K;
-  float *begin_b = B + bx * BLOCK;
-  float *end_a = begin_a + K;
+  const int col = blockIdx.x * BLOCK + threadIdx.x;
+  const int row = blockIdx.y * BLOCK + threadIdx.y;
+  const int ty = threadIdx.y;
+  const int tx = threadIdx.x;
 
   float sum = 0.0f;
-  for (auto a_ptr = begin_a, b_ptr = begin_b; a_ptr < end_a;
-       a_ptr += BLOCK, b_ptr += BLOCK * N) {
-    __shared__ float shared_a[BLOCK][BLOCK];
-    __shared__ float shared_b[BLOCK][BLOCK];
-    shared_a[ty][tx] = a_ptr[ty * K + tx];
-    shared_b[ty][tx] = b_ptr[ty * N + tx];
+  for (int ph = 0; ph < (n + BLOCK - 1) / BLOCK; ++ph) {
+
+    int a_col = ph * BLOCK + tx;
+    int a_idx = row * n + a_col;
+
+    int b_row = ph * BLOCK + ty;
+    int b_idx = b_row * k + col;
+
+    if (row < m && a_col < n) {
+      shmem_a[ty * BLOCK + tx] = matrix_a[a_idx];
+    } else {
+      shmem_a[ty * BLOCK + tx] = 0.0f;
+    }
+
+    if (b_row < n && col < k) {
+      shmem_b[ty * BLOCK + tx] = matrix_b[b_idx];
+    } else {
+      shmem_b[ty * BLOCK + tx] = 0.0f;
+    }
+
     __syncthreads();
 
-#pragma unroll
-    for (int k = 0; k < BLOCK; ++k) {
-      sum += shared_a[ty][k] * shared_b[k][tx];
+    for (int i = 0; i < BLOCK; ++i) {
+      sum += shmem_a[ty * BLOCK + i] * shmem_b[i * BLOCK + tx];
     }
 
     __syncthreads();
   }
 
-  C[(BLOCK * by + ty) * N + BLOCK * bx + tx] = sum;
+  if (row < m && col < k) {
+    matrix_c[row * k + col] = sum;
+  }
 }
 
 void matmul_native(const float *a, const float *b, float *c, int m, int n,
@@ -53,7 +73,7 @@ void matmul_native(const float *a, const float *b, float *c, int m, int n,
   float *dev_a = nullptr;
   auto err = cudaMalloc(&dev_a, m * n * sizeof(float));
   err = cudaMemcpy(dev_a, a, m * n * sizeof(float), cudaMemcpyHostToDevice);
-  
+
   float *dev_b = nullptr;
   err = cudaMalloc(&dev_b, n * k * sizeof(float));
   err = cudaMemcpy(dev_b, b, n * k * sizeof(float), cudaMemcpyHostToDevice);
@@ -66,9 +86,53 @@ void matmul_native(const float *a, const float *b, float *c, int m, int n,
   dim3 block(THREAD_COUNT, THREAD_COUNT);
   dim3 grid((k + block.x - 1) / block.x, (m + block.y - 1) / block.y);
 
-  matmul_native_kernel<<<grid,block>>>(dev_a, dev_b, dev_c, m, n, k);
+  matmul_native_kernel<<<grid, block>>>(dev_a, dev_b, dev_c, m, n, k);
 
   cudaMemcpy(c, dev_c, m * k * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_a);
+  cudaFree(dev_b);
+  cudaFree(dev_c);
 }
 
-void matmul_sharedmemory(const float *a, const float *b, float *c, int m, int n, int k) {}
+void matmul_sharedmemory(const float *matrix_a, const float *matrix_b,
+                         float *matrix_c, int m, int n, int k) {
+  float *dev_a = nullptr;
+  auto err = cudaMalloc(&dev_a, m * n * sizeof(float));
+  err = cudaMemcpy(dev_a, matrix_a, m * n * sizeof(float),
+                   cudaMemcpyHostToDevice);
+
+  float *dev_b = nullptr;
+  err = cudaMalloc(&dev_b, n * k * sizeof(float));
+  err = cudaMemcpy(dev_b, matrix_b, n * k * sizeof(float),
+                   cudaMemcpyHostToDevice);
+
+  float *dev_c = nullptr;
+  err = cudaMalloc(&dev_c, m * k * sizeof(float));
+  err = cudaMemcpy(dev_c, matrix_c, m * k * sizeof(float),
+                   cudaMemcpyHostToDevice);
+
+  const int BLOCK_SIZE = 32;
+  dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 grid((k + block.x - 1) / block.x, (m + block.y - 1) / block.y);
+
+  // calculate shared memory size
+  auto shared_mem_size = 2 * BLOCK_SIZE * BLOCK_SIZE * sizeof(float);
+
+  if (BLOCK_SIZE == 16) {
+    matmul_sharedmemory_kernel<16>
+        <<<grid, block, shared_mem_size>>>(dev_a, dev_b, dev_c, m, n, k);
+  } else if (BLOCK_SIZE == 32) {
+    matmul_sharedmemory_kernel<32>
+        <<<grid, block, shared_mem_size>>>(dev_a, dev_b, dev_c, m, n, k);
+  } else {
+    matmul_sharedmemory_kernel<8>
+        <<<grid, block, shared_mem_size>>>(dev_a, dev_b, dev_c, m, n, k);
+  }
+
+  cudaMemcpy(matrix_c, dev_c, m * k * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_a);
+  cudaFree(dev_b);
+  cudaFree(dev_c);
+}
