205173b59355b0e7e2fdd7375cdd371ccad34efd
GaoXiangYa <gx19991105@outlook.com>
2025-08-28 22:20:56 +0800

    fix reduce_v0 bug

--- src/cuda/reduce/reduce_v0.cu
+++ src/cuda/reduce/reduce_v0.cu
@@ -4,23 +4,24 @@
 #include <cuda_runtime.h>
 
 __global__ void reduce_kernel_v0(float *input, float *output) {
-  const int i = 2 * threadIdx.x;
-  for (int stride = 1; stride <= blockDim.x; stride *= 2) {
-    if (threadIdx.x % stride == 0) {
+  const int segment = blockIdx.x * blockDim.x;
+  const int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  const int i = threadIdx.x + segment;
+  for (int stride = blockDim.x / 2; stride >= 1; stride >>= 1) {
+    if (threadIdx.x < stride) {
       input[i] += input[i + stride];
     }
     __syncthreads();
   }
   if (threadIdx.x == 0) {
-    output[blockIdx.x] = input[0];
+    output[blockIdx.x] = input[tid];
   }
 }
 
 __host__ void reduce_v0(float *input, size_t input_count, float *output) {
   size_t input_size = input_count * sizeof(float);
   const int THREAD_COUNT = 32;
-  const int elements_per_block = 2 * THREAD_COUNT;
-  const int BLOCK_COUNT = (input_count + elements_per_block- 1) / elements_per_block;
+  const int BLOCK_COUNT = (input_count + THREAD_COUNT - 1) / (THREAD_COUNT);
 
   size_t output_size = BLOCK_COUNT * sizeof(float);
 
@@ -33,14 +34,15 @@ __host__ void reduce_v0(float *input, size_t input_count, float *output) {
   err = cudaMalloc(&output_dev, output_size);
   cudaMemcpy(output_dev, output, output_size,
              cudaMemcpyKind::cudaMemcpyHostToDevice);
-  float* output_host = (float*)malloc(output_size);
+  float *output_host = (float *)malloc(output_size);
 
   reduce_kernel_v0<<<BLOCK_COUNT, THREAD_COUNT>>>(input_dev, output_dev);
 
   cudaMemcpy(output_host, output_dev, output_size,
              cudaMemcpyKind::cudaMemcpyDeviceToHost);
+
   float sum = 0.0f;
-  for (int i = 0; i < BLOCK_COUNT; ++ i) {
+  for (int i = 0; i < BLOCK_COUNT; ++i) {
     sum += output_host[i];
   }
 
