f69d5e121628c71fdd9cdf6e92ff808fc1fc51b4
GaoXiangYa <gx19991105@outlook.com>
2025-09-03 22:44:42 +0800

    add reduce kernel v3

--- src/cuda/reduce/reduce_benchmark.cpp
+++ src/cuda/reduce/reduce_benchmark.cpp
@@ -1,13 +1,14 @@
 #include "reduce.h"
 
 int main() {
-  // reduce v0 benchmark
-  { reduce_v0_benchmark(); }
+  // // reduce v0 benchmark
+  // { reduce_v0_benchmark(); }
 
-  // reduce v1 benchmark
-  { reduce_v1_benchmark(); }
+  // // reduce v1 benchmark
+  // { reduce_v1_benchmark(); }
 
-  { reduce_v2_benchmark(); }
+  // { reduce_v2_benchmark(); }
 
+  { reduce_v3_benchmark(); }
   return 0;
 }
---
+++ src/cuda/reduce/reduce_v3.cu
@@ -0,0 +1,109 @@
+#include <cuda_runtime.h>
+#include <vector>
+#include "util.h"
+#include "benchmark.cuh"
+
+__device__ void wrapReduce(volatile float *cache, int tid) {
+  cache[tid] += cache[tid + 32];
+  cache[tid] += cache[tid + 16];
+  cache[tid] += cache[tid + 8];
+  cache[tid] += cache[tid + 4];
+  cache[tid] += cache[tid + 2];
+  cache[tid] += cache[tid + 1];
+}
+
+template <int SHARED_MEM_SIZE, int COARSE_FACTOR>
+__global__ void reduce_kernel_v3(float *input, float *output) {
+  __shared__ float shmem[SHARED_MEM_SIZE];
+
+  const int segment = COARSE_FACTOR * blockDim.x * blockIdx.x;
+  const int tx = threadIdx.x;
+  const int i = segment + tx;
+  float sum = input[i];
+
+#pragma unroll
+  for (int tile = 1; tile < COARSE_FACTOR; ++tile) {
+    sum += input[i + tile * SHARED_MEM_SIZE];
+  }
+  shmem[tx] = sum;
+
+  for (int stride = blockDim.x / 2; stride > 32; stride >>= 1) {
+    __syncthreads();
+    if (tx < stride) {
+      shmem[tx] += shmem[tx + stride];
+    }
+  }
+
+  if (tx < 32) {
+    wrapReduce(shmem, tx);
+  }
+
+  if (tx == 0) {
+    output[blockIdx.x] = shmem[0];
+  }
+}
+
+void reduce_v3(float *input, size_t input_count, float *output) {
+  size_t input_size = input_count * sizeof(float);
+  const int THREAD_COUNT = 32;
+  const int COARSE_FACTOR = 4;
+  const int BLOCK_COUNT = (input_count + THREAD_COUNT * COARSE_FACTOR - 1) /
+                          (THREAD_COUNT * COARSE_FACTOR);
+
+  size_t output_size = BLOCK_COUNT * sizeof(float);
+
+  float *input_dev = nullptr;
+  auto err = cudaMalloc(&input_dev, input_size);
+  cudaMemcpy(input_dev, input, input_size,
+             cudaMemcpyKind::cudaMemcpyHostToDevice);
+
+  float *output_dev = nullptr;
+  err = cudaMalloc(&output_dev, output_size);
+  cudaMemcpy(output_dev, output, output_size,
+             cudaMemcpyKind::cudaMemcpyHostToDevice);
+  float *output_host = (float *)std::malloc(output_size);
+
+  reduce_kernel_v3<THREAD_COUNT, COARSE_FACTOR>
+      <<<BLOCK_COUNT, THREAD_COUNT>>>(input_dev, output_dev);
+
+  cudaMemcpy(output_host, output_dev, output_size,
+             cudaMemcpyKind::cudaMemcpyDeviceToHost);
+
+  float sum = 0.0f;
+  for (int i = 0; i < BLOCK_COUNT; ++i) {
+    sum += output_host[i];
+  }
+
+  *output = sum;
+}
+
+void reduce_v3_benchmark() {
+  const size_t count = 512 * 1024 * 1024;
+  const size_t input_size = count * sizeof(float);
+  const int repeat = 10000;
+
+  std::vector<float> input(count, 0.0f);
+  init_random(input);
+  const int THREAD_COUNT = 64;
+  const int COARSE_FACTOR = 4;
+  const int BLOCK_COUNT = (count + THREAD_COUNT * COARSE_FACTOR - 1) /
+                          (THREAD_COUNT * COARSE_FACTOR);
+  std::vector<float> output(BLOCK_COUNT, 0.0f);
+
+  float *input_dev = nullptr;
+  auto err = cudaMalloc(&input_dev, input_size);
+  float *output_dev = nullptr;
+  err = cudaMalloc(&output_dev, BLOCK_COUNT * sizeof(float));
+
+  cudaMemcpy(input_dev, input.data(), input_size,
+             cudaMemcpyKind::cudaMemcpyHostToDevice);
+  cudaMemcpy(output_dev, output.data(), BLOCK_COUNT * sizeof(float),
+             cudaMemcpyKind::cudaMemcpyHostToDevice);
+
+  double flops = 1.0 * count;
+  double bytes = 2.0 * input_size;
+
+  benchmarkKernel("reduce_kernel_v3<32, 32>", BLOCK_COUNT, THREAD_COUNT, flops,
+                  bytes, repeat, reduce_kernel_v3<THREAD_COUNT, COARSE_FACTOR>,
+                  input_dev, output_dev);
+}
