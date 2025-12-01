07978992afc7265c5115786dff21fb8b26f69e71
GaoXiangYa <gx19991105@outlook.com>
2025-12-01 22:07:18 +0800

    fix gemv_v4 unit test

--- src/cuda/gemv/test.cpp
+++ src/cuda/gemv/test.cpp
@@ -111,11 +111,11 @@ TEST(GEMV, gemv_v4) {
   std::vector<float> vec_x(n, 0.0f);
   std::vector<float> vec_y(m, 0.0f);
   std::vector<float> cutlass_vec_y(m, 0.0f);
-  const float alpha = 1.0f, beta = 0.0f;
+  const float alpha = 1.0f, beta = -0.01f;
 
-  init_random(mat_a, 1.0f, 1.0f);
-  init_random(vec_x,1.0f, 1.0f);
-  init_random(vec_y,1.0f, 1.0f);
+  init_random(mat_a, -1.0f, 1.0f);
+  init_random(vec_x,-1.0f, 1.0f);
+  init_random(vec_y,-1.0f, 1.0f);
 
   std::copy(vec_y.begin(), vec_y.end(), cutlass_vec_y.begin());
 
@@ -123,7 +123,7 @@ TEST(GEMV, gemv_v4) {
   cutlass_gemv_fp32(mat_a.data(), vec_x.data(), cutlass_vec_y.data(), m, n,
                     alpha, beta);
 
-  const float tolerance = 0.0001f;
+  const float tolerance = 0.01f;
   for (int i = 0; i < m; ++i) {
     // std::cout << cutlass_vec_y[i] << " " << vec_y[i] << "\n";
     if (std::abs(cutlass_vec_y[i] - vec_y[i]) >= tolerance) {
