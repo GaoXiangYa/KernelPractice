f30967b77f2fb229199abbeef0c07dab1f0a1684
GaoXiangYa <gx19991105@outlook.com>
2026-04-03 15:11:48 +0800

    [OpenCL]: add valid_conv2d_v3_kernel

--- src/opencl/conv2d/conv2d.cpp
+++ src/opencl/conv2d/conv2d.cpp
@@ -120,13 +120,61 @@ void valid_conv2d_v2(const float* input, const float* filter, float* output,
   ocl_kernel.GetKernel().setArg(1, buffer_filter);
   ocl_kernel.GetKernel().setArg(2, buffer_output);
   ocl_kernel.GetKernel().setArg(
-      3, sizeof(float) * kSharedInputSizeX * kSharedInputSizeY, nullptr);
+      3, sizeof(float) * (kSharedInputSizeX + 1) * kSharedInputSizeY, nullptr);
   ocl_kernel.GetKernel().setArg(4, input_rows);
   ocl_kernel.GetKernel().setArg(5, input_cols);
   ocl_kernel.GetKernel().setArg(6, kernel_rows);
   ocl_kernel.GetKernel().setArg(7, kernel_cols);
   ocl_kernel.GetKernel().setArg(8, kOutputRows);
   ocl_kernel.GetKernel().setArg(9, kOutputCols);
+  decltype(auto) queue = ocl_kernel.GetCommandQueue();
+  decltype(auto) kernel = ocl_kernel.GetKernel();
+
+  cl::Event event;
+  ocl_kernel.GetCommandQueue()->enqueueNDRangeKernel(kernel, cl::NullRange,
+                                                     global_work_size,
+                                                     local_work_size, nullptr,
+                                                     &event);
+  queue->enqueueReadBuffer(buffer_output, CL_TRUE, 0,
+                           sizeof(float) * kOutputRows * kOutputCols, output);
+  ocl_kernel.ProflingKernel(event);
+}
+
+void valid_conv2d_v3(const float* input, const float* filter, float* output,
+                     int input_rows, int input_cols, int kernel_rows,
+                     int kernel_cols) {
+  const std::string build_options = "";
+  OCLKernel ocl_kernel("../src/opencl/conv2d/valid_conv2d_v3.cl",
+                       "valid_conv2d_v3_kernel", build_options);
+  const int kOutputRows = input_rows - kernel_rows + 1;
+  const int kOutputCols = input_cols - kernel_cols + 1;
+  const int kRegX = 2;
+  const int kRegY = 2;
+  const int kLocalSizeX = 16;
+  const int kLocalSizeY = 16;
+
+  cl::NDRange global_work_size((kOutputCols - (kLocalSizeX * kRegX) + 1) /
+                                   (kLocalSizeX * kRegX) * kLocalSizeX,
+                               (kOutputRows - (kLocalSizeY * kRegX) + 1) /
+                                   (kLocalSizeY * kRegY) * kLocalSizeY);
+  cl::NDRange local_work_size(kLocalSizeX, kLocalSizeY);
+
+  cl::Buffer buffer_input(ocl_kernel.GetKernelContext(),
+                          CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                          sizeof(float) * input_rows * input_cols,
+                          (void*) input);
+  cl::Buffer buffer_filter(ocl_kernel.GetKernelContext(),
+                           CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                           sizeof(float) * kernel_rows * kernel_cols,
+                           (void*) filter);
+  cl::Buffer buffer_output(ocl_kernel.GetKernelContext(),
+                           CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+                           sizeof(float) * kOutputRows * kOutputCols,
+                           (void*) output);
+
+  ocl_kernel.set_kernel_args(0, buffer_input, buffer_filter, buffer_output,
+                             input_rows, input_cols, kernel_rows, kernel_cols,
+                             kOutputCols, kOutputRows);
   decltype(auto) queue = ocl_kernel.GetCommandQueue();
   decltype(auto) kernel = ocl_kernel.GetKernel();
 
---
+++ src/opencl/conv2d/valid_conv2d_v3.cl
@@ -0,0 +1,65 @@
+// valid conv2d + constant memory + register blocking (each thread calculate 2x2
+// output)
+#define REG_X 2
+#define REG_Y 2
+
+__kernel void valid_conv2d_v3_kernel(
+    __global const float* restrict input, __constant float* restrict filter,
+    __global float* restrict output, const int input_rows, const int input_cols,
+    const int filter_rows, const int filter_cols, const int output_rows,
+    const int output_cols) {
+  const int local_size_x = get_local_size(0);
+  const int local_size_y = get_local_size(1);
+
+  const int local_x = get_local_id(0);
+  const int local_y = get_local_id(1);
+
+  const int global_output_x_base = get_group_id(0) * local_size_x + REG_X;
+  const int global_output_y_base = get_global_id(0) * local_size_y + REG_Y;
+
+  float sum[REG_Y][REG_X];
+#pragma unroll
+  for (int reg_y = 0; reg_y < REG_Y; ++reg_y) {
+#pragma unroll
+    for (int reg_x = 0; reg_x < REG_X; ++reg_x) {
+      sum[reg_y][reg_x] = 0.0f;
+    }
+  }
+
+#pragma unroll
+  for (int reg_y = 0; reg_y < REG_Y; ++reg_y) {
+#pragma unroll
+    for (int reg_x = 0; reg_x < REG_X; ++reg_x) {
+      for (int filter_y = 0; filter_y < filter_rows; ++filter_y) {
+        for (int filter_x = 0; filter_x < filter_cols; ++filter_x) {
+          const int global_output_x = global_output_x_base + reg_x + local_x;
+          const int global_output_y = global_output_y_base + reg_y + local_y;
+          const int global_input_x = global_output_x + filter_x;
+          const int global_input_y = global_output_y + filter_y;
+
+          if (global_input_x < input_cols && global_input_y < input_rows &&
+              global_output_x < output_cols && global_output_y < output_rows) {
+            float val = input[global_input_y * input_cols + global_input_x];
+            float f = filter[filter_y * filter_cols + filter_x];
+
+            sum[reg_y][reg_x] += f;
+          }
+        }
+      }
+    }
+  }
+
+#pragma unroll
+  for (int reg_y = 0; reg_y < REG_Y; ++reg_y) {
+    const int global_output_y = global_output_y_base + reg_y + local_y;
+    if (global_output_y < output_rows) {
+#pragma unroll
+      for (int reg_x = 0; reg_x < REG_X; ++reg_x) {
+        const int global_output_x = global_output_x_base + reg_x + local_x;
+        if (global_output_x < output_cols) {
+          output[global_output_y * output_cols + global_output_x] = sum[reg_y][reg_x];
+        }
+      }
+    }
+  }
+}
